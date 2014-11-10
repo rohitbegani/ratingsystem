e45eba155a4c581569b242254a8258f191ada3c0
gautamsawhney <gsawhney21@gmail.com>
2014-11-10 19:35:05 +0530

    Minor changes

--- convert_json_to_hash.rb
+++ convert_json_to_hash.rb
@@ -1,7 +1,11 @@
 require 'json'
 
-req_hash = File.read("file.txt").split("\n").map do |line|
+req_hash = File.read("test123.json").split("\n").map do |line|
   			 JSON.parse(line)
 		   end
 
-puts req_hash.first
+a = req_hash.first
+
+puts a['business_id']                  
+
+# Do same for whatever you may want to retrieve from the hash.

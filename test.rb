804dbb71111985ecbd0c7055836840abe244e398
gautamsawhney <gsawhney21@gmail.com>
2014-11-11 13:34:21 +0530

    scaled values formulas

--- test.rb
+++ test.rb
@@ -1,13 +1,6 @@
 string = "I'm sorry, I'm Legend #legend
           extremely busy right now. I just 
-          looked at the clock, and it's 12:54 AM,
-          I've still got a lot of work to do. 
-          Don't worry about the event tomorrow, 
-          it's been moved ahead a week, the 28th of december.
-          Remember though, you've got to call to get a ticket 
-          soon, their # is 212-323-1239 and they live at Los 
-          Angeles, or call the toll free number 1-800-567-4321.
-          Their website says it costs $23 per person."
+          filthy worst ok love fav"
 
 positiverange4 = ['legendary', 'legend', 'finest', 'insane', 'best'];
 positiverange3 = ['favorite', 'favourite', 'fav', 'delicious', 'awesome', 'perfect', 'perfection', 'perfectly', 'scrumptous'];
@@ -21,13 +14,50 @@ negativerange4 = ['worst', 'terrible', 'horrible', 'disgusting'];
 
 q = string.downcase.gsub(/[^A-Za-z0-9\s]/,"")
 words = q.split(" ") 
+
+val = 0
+count = 0
+words.each do |w|
+  if positiverange4.include? w
+   val += 4
+   count += 1
+  elsif positiverange3.include? w
+   val += 3
+   count += 1
+  elsif positiverange2.include? w
+   val += 2
+   count += 1
+  elsif positiverange1.include? w
+   val += 1
+   count += 1
+  elsif neutralrange.include? w
+   val += 0
+   count += 1
+  elsif negativerange1.include? w
+   val += -1 
+   count += 1   
+  elsif negativerange2.include? w
+   val += -2
+   count += 1
+  elsif negativerange3.include? w
+   val += -3
+   count += 1
+  elsif negativerange4.include? w
+   val += -4 
+   count += 1 
+  end
+   
+   			
+end
+puts  val/count.to_f
+	
+
 #q = words.downcase
 
 # q.each do |w|
 #      w.downcase
 # end
 
-puts words
 
 # words2.each do |t|
 #   words.each do |u|

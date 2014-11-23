548dab7e29f7cc3d02a7f161bca34b5ebe8571e2
rohitbegani <rbegani72@gmail.com>
2014-11-23 17:48:51 +0530

    More Changes

--- test.rb
+++ test.rb
@@ -107,21 +107,21 @@ DataMapper.setup :default, "sqlite://#{Dir.pwd}/ratingsystem.db"
 
 	    ###Adding formula to scale and extract exact rating #Check veracity of formula
 	    ### Why is this <> not working?
-	    if -4 < x < -3.5
+	    if x > -4 and x < -3.5
 	    	rating_integer = 1
-	    elsif -3.5 < x < -2.5
+	    elsif x > -3.5 and x < -2.5
 	    	rating_integer = 1.5
-	    elsif -2.5 < x < -1.5
+	    elsif x > -2.5 and x < -1.5
 	    	rating_integer = 2
-	    elsif -1.5 < x < 0.5
+	    elsif x > -1.5 and x < 0.5
 	    	rating_integer = 2.5
-	    elsif 0.5 < x < 1.5
+	    elsif x > 0.5 and x < 1.5
 	    	rating_integer = 3
-	    elsif 1.5 < x < 2.5
+	    elsif x > 1.5 and x < 2.5
 	    	rating_integer = 3.5
-	    elsif 2.5 < x < 3.5
+	    elsif x > 2.5 and x < 3.5
 	    	rating_integer = 4
-	    elsif 3.5 < x < 4.5
+	    elsif x > 3.5 and x < 4.5
 	    	rating_integer = 4.5
 	    else
 	    	rating_integer = 5
